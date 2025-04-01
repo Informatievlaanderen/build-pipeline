210551d8cdcb0616e0337944ca26b755a99dfe1a
Arne Dumarey <dumarey.arne@gmail.com>
2025-04-01 16:02:53 +0200

    fix: add argument for build dir in scripts

--- generic/ci-confluence.sh
+++ generic/ci-confluence.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-python packages/Be.Vlaanderen.Basisregisters.Build.Pipeline/Content/ci-md2conf.py CHANGELOG.md VBR \
+BUILD_DIR="${1:-.build}"
+
+python $BUILD_DIR/Be.Vlaanderen.Basisregisters.Build.Pipeline/Content/ci-md2conf.py CHANGELOG.md VBR \
     --user "${CONFLUENCE_USERNAME}" \
     --password "${CONFLUENCE_PASSWORD}" \
     --orgname "vlaamseoverheid" \
--- generic/ci-jira.sh
+++ generic/ci-jira.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-python packages/Be.Vlaanderen.Basisregisters.Build.Pipeline/Content/ci-jiraversion.py ${JIRA_PREFIX}-${JIRA_VERSION} ${JIRA_PROJECT} \
+BUILD_DIR="${1:-.build}"
+
+python $BUILD_DIR/Be.Vlaanderen.Basisregisters.Build.Pipeline/Content/ci-jiraversion.py ${JIRA_PREFIX}-${JIRA_VERSION} ${JIRA_PROJECT} \
     --user "${CONFLUENCE_USERNAME}" \
     --password "${CONFLUENCE_PASSWORD}" \
     --orgname "vlaamseoverheid" \
